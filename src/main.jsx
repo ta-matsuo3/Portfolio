a47431cf8676ac08e41b7636dc648dcb7174b616
ta-matsuo3 <t.matsuo.recurrent@gmail.com>
2025-11-20 11:27:33 +0900

    010_変更点プッシュ

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Home from './compornent/_main/Home/Home.jsx'
-import Works from './compornent/_main/Works/Works.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
